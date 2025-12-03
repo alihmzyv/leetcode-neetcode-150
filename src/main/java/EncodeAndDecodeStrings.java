44cce793c8efead06d30616fbb5b029707819011
Ali Hamzayev <alihmzyv@gmail.com>
2025-12-03 17:58:32 +0400

    solve encode and decode strings.

---
+++ src/main/java/EncodeAndDecodeStrings.java
@@ -0,0 +1,33 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class EncodeAndDecodeStrings {
+    private static final char DELIMITER = '#';
+
+    public String encode(List<String> strs) {
+        StringBuilder sb = new StringBuilder();
+
+        strs.forEach(str -> {
+            sb.append(str.length())
+                    .append(DELIMITER)
+                    .append(str);
+        });
+
+        return sb.toString();
+    }
+
+    public List<String> decode(String str) {
+        List<String> result = new LinkedList<>();
+        int endOfPrevious = -1;
+        int length = str.length();
+        for (int i = 0; i < length; i++) {
+            if (str.charAt(i) == DELIMITER) {
+                int lengthOfStr = Integer.parseInt(str.substring(endOfPrevious + 1, i));
+                result.add(str.substring(i + 1, i + 1 + lengthOfStr));
+                endOfPrevious = i + lengthOfStr;
+                i += (1 + lengthOfStr);
+            }
+        }
+        return result;
+    }
+}
---
+++ src/test/java/EncodeAndDecodeStringsTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EncodeAndDecodeStringsTest {
+    @Test
+    void testEncode() {
+        assertEquals("4#neet4#code4#love3#you", new EncodeAndDecodeStrings().encode(List.of("neet","code","love","you")));
+    }
+
+    @Test
+    void testDecode() {
+        assertEquals(List.of("neet","code","love","you"), new EncodeAndDecodeStrings().decode("4#neet4#code4#love3#you"));
+    }
+}
