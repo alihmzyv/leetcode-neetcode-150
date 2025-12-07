ed3a91415a033f3d51eb95224ebbc65c47d6461f
Ali Hamzayev <alihmzyv@gmail.com>
2025-12-07 16:27:31 +0400

    add all solutions of valid sudoku.

--- src/main/java/ValidSudoku.java
+++ src/main/java/ValidSudoku.java
@@ -1,7 +1,10 @@
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class ValidSudoku {
+    //time - O(n^2), space - O(n)
     public boolean isValidSudokuBruteForce(char[][] board) {
         //go through rows
         int size = board.length;
@@ -55,25 +58,63 @@ public class ValidSudoku {
         return true;
     }
 
-    //space - O(n2), worse than the one above
+    //space - O(n2), worse than the one above in terms of space
     public boolean isValidSudokuOnePass(char[][] board) {
         int size = board.length;
-        Set[] rows = new Set[size];
-        Set[] cols = new Set[size];
-        Set[] boxes = new Set[size];
+        int boxSize = (int) Math.sqrt(size);
+        Map<Integer, Set<Character>> rows = new HashMap<>();
+        Map<Integer, Set<Character>> cols = new HashMap<>();
+        Map<Integer, Set<Character>> boxes = new HashMap<>();
 
         for (int row = 0; row < size; row++) {
             for (int col = 0; col < size; col++) {
                 char num = board[row][col];
-                if (rows[row].contains(num)) {
-                    return false;
+                if (num != '.') {
+                    if (rows.computeIfAbsent(row, n -> new HashSet<>()).contains(num)) {
+                        return false;
+                    }
+                    if (cols.computeIfAbsent(col, n -> new HashSet<>()).contains(num)) {
+                        return false;
+                    }
+                    //neetcode uses a string of "row,col", instead i calculate an index,
+                    //going through rows left to right, starting at zero
+                    int idxOfBox = (row / boxSize) * boxSize + col / boxSize;
+                    if (boxes.computeIfAbsent(idxOfBox, n -> new HashSet<>()).contains(num)) {
+                        return false;
+                    }
+                    rows.get(row).add(num);
+                    cols.get(col).add(num);
+                    boxes.get(idxOfBox).add(num);
                 }
-                if (cols[col].contains(num)) {
-                    return false;
-                }
-                //TODO: look into this and the other solution
             }
         }
-        return false;
+        return true;
+    }
+
+    //time - O(n^2), space - O(n), super fast due to not hashing anything
+    public boolean isValidSudokuBitMaskOnePass(char[][] board) {
+        int size = board.length;
+        int boxSize = (int) Math.sqrt(size);
+        int[] rows = new int[size];
+        int[] cols = new int[size];
+        int[] boxes = new int[size];
+
+        for (int row = 0; row < size; row++) {
+            for (int col = 0; col < size; col++) {
+                char num = board[row][col];
+                if (num != '.') {
+                    int index = 1 << (num - '1');
+                    int boxIndex = (row / boxSize) * boxSize + (col / boxSize);
+                    if ((rows[row] & index) > 0 || (cols[col] & index) > 0 ||
+                            (boxes[boxIndex] & index) > 0) {
+                        return false;
+                    }
+                    rows[row] |= index;
+                    cols[col] |= index;
+                    boxes[boxIndex] |= index;
+                }
+            }
+        }
+        return true;
     }
 }
--- src/test/java/ValidSudokuTest.java
+++ src/test/java/ValidSudokuTest.java
@@ -21,4 +21,22 @@ class ValidSudokuTest {
         ValidSudoku validSudoku = new ValidSudoku();
         assertTrue(validSudoku.isValidSudokuBruteForce(board));
     }
+
+    @Test
+    void isValidSudokuOnePass() {
+        char[][] board = {
+                {'5','3','.','.','7','.','.','.','.'},
+                {'6','.','.','1','9','5','.','.','.'},
+                {'.','9','8','.','.','.','.','6','.'},
+                {'8','.','.','.','6','.','.','.','3'},
+                {'4','.','.','8','.','3','.','.','1'},
+                {'7','.','.','.','2','.','.','.','6'},
+                {'.','6','.','.','.','.','2','8','.'},
+                {'.','.','.','4','1','9','.','.','5'},
+                {'.','.','.','.','8','.','.','7','9'}
+        };
+
+        ValidSudoku validSudoku = new ValidSudoku();
+        assertTrue(validSudoku.isValidSudokuOnePass(board));
+    }
 }
